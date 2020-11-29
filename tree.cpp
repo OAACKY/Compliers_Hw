ec7eeac2ae6acb769eb3ba8bda198d4b9d6ff965
cky <553512396@qq.com>
2020-11-29 23:59:25 +0800

    add tree.cpp

---
+++ tree.cpp
@@ -0,0 +1,32 @@
+#include "tree.h"
+
+void addChild(TreeNode *){
+
+}
+
+void addSibling(TreeNode *){
+
+}
+
+void genNodeId(){
+
+}
+
+void printAST(){
+
+}
+
+void printNodeInfo(){
+
+}
+void printNodeConnection(){
+
+}
+
+string nodeTypeInfo(){
+
+}
+
+TreeNode(NodeType type){
+    
+}
